ea5656cec5d9705eb138b854177fa768aa3b2e48
totallytikl <tpoot96@gmail.com>
2016-02-20 22:47:23 +0100

    Removed test printf, added argc=0 to overwrite previous argv

--- main.c
+++ main.c
@@ -11,23 +11,26 @@ const char *mypath[] = {
 };
 
 int main (){
-  char *path = "~/home";
+  char *path = "/home";
   char input[1024];
-  char *arg[10];
+  char *argv[10];
   int argc = 0;
+  //int i; still needed ???
   
   while (1){
     /* Wait for input */
-    printf ("prompt> %s ", path);
+    printf ("prompt>%s ", path);
     fgets (input, sizeof(input), stdin);
     
     
     /* Parse input */ 
-    char * iptr = input; //input pointer
-    while ((argv[argc] = strsep(&iptr, " ")) != NULL)
-    {
-      printf("%s", argv[argc]);
+    char * iptr = input; //input pointer needed for strsep argument
+    while ((argv[argc] = strsep(&iptr, " ")) != NULL){
       argc++;
+    }  
+
+    if(strcmp(argv[0], "cd") == 0){
+      printf("test");
     }
 
     /* If necessary locate executable using mypath array 
@@ -39,6 +42,8 @@ int main (){
     }else{
       wait (...);
     }*/
+
+    argc = 0;
   }
   return 0;
 }
