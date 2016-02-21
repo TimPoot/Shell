d2bd3b91219cfcd4b18fca1bbda70bc9fb77b9f5
totallytikl <tpoot96@gmail.com>
2016-02-21 16:30:38 +0100

    Added exit functionality

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 
 const char *mypath[] = {
   "./",
@@ -15,10 +16,10 @@ int main (){
   char input[1024];
   char *argv[10];
   int argc = 0;
-  //int i; still needed ???
+  int exit = 1;
 
   strcpy(path, mypath[1]);
-  while (1){
+  while (exit){
     /* Wait for input */
     printf ("prompt >%s ", path);
     fgets (input, sizeof(input), stdin);
@@ -31,12 +32,15 @@ int main (){
     }
     argv[argc-1][strlen(argv[argc-1])-1] = 0; //remove last \n from string 
 
-    if(strcmp(argv[0], "cd") == 0){
+
+    if(strcmp(argv[0], "cd") == 0){ //cd 
       if(argc == 2){
         strcat(path, argv[1]);
       }else{
         printf("Please only enter 2 arguments\n");
       }
+    }else if(strcmp(argv[0], "exit") == 0){
+      exit = 0;
     }
 
     /* If necessary locate executable using mypath array 
